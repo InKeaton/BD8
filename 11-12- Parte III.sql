1fd5b319d3d3fd53d14a577010969f09a91e70b4
InKeaton <edo.vas@protonmail.com>
2023-07-14 11:25:49 +0200

    reorganized files, added part 3

--- 11-12- Parte III.sql
+++ 11-12- Parte III.sql
@@ -1,3 +1,4 @@
+
 ---------------------------------------- CARICO DI LAVORO -----------------------------------------
 
 /*
@@ -63,138 +64,22 @@ USING HASH (nome);
 
 ---------------------------------------- POLITICA D'ACCESSO -----------------------------------------
 
-/*AG
-AL
-AN
-AO
-AR
-AP
-AT
-AV
-BA
-BT
-BL
-BN
-BG
-BI
-BO
-BZ
-BS
-BR
-CA
-CL
-CB
-CI
-CE
-CT
-CZ
-CH
-CH
-CO
-CS
-CR
-KR
-CN
-EN
-FM
-FE
-FI
-FG
-FC
-FR
-GE
-GO
-GR
-IM
-IS
-SP
-AQ
-LT
-LE
-LC
-LI
-LO
-LU
-MC
-MN
-MS
-MT
-ME
-MI
-MO
-MB
-NA
-NO
-NU
-OT
-OR
-PD
-PA
-PR
-PV
-PG
-PU
-PE
-PC
-PI
-PT
-PN
-PZ
-PO
-RG
-RA
-RC
-RE
-RI
-RN
-RM
-RO
-SA
-VS
-SS
-SV
-SI
-SR
-SO
-TA
-TE
-TR
-TO
-OG
-TP
-TN
-TV
-TS
-UD
-VA
-VE
-VB
-VC
-VR
-VV
-VI
-VT
-*/
+/*
+ Creazione Ruoli
+ */
+
+CREATE ROLE Studente;
+CREATE ROLE Docente;
+CREATE ROLE ReferenteScuola;
+CREATE ROLE ReferenteIstituto;
+CREATE ROLE GestoreGlobaleProgetto;
+
+/*
+ Assegnazione Permessi
+ */
 
 
-set search_path to "orti1";
 
-
-/*SELECT relpages FROM pg_class WHERE relname = 'finanziamento';
-
-CREATE INDEX test1
-ON Finanziamento (entità);
-
-CLUSTER ON test1;
-
-ANALYZE;
-
-SELECT istituto, progetto
-FROM Finanziamento
-WHERE (progetto = 'PON EduGreen' OR progetto = 'Sviluppo Base Dati') AND
-       entità > 1000;
-*/
-
-
-/* BEFORE: SEQ SCAN : 0.257 */
-/* AFTER: INDEX SCAN : 0.094 */
+/*
+ Creazione Utenti ed Assegnazione Ruoli
+ */
