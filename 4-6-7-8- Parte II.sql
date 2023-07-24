8a4dc8a333f76677d69ff107521297be6a236f27
InKeaton <edo.vas@protonmail.com>
2023-07-24 09:12:32 +0200

    added provincia check

--- 4-6-7-8- Parte II.sql
+++ 4-6-7-8- Parte II.sql
@@ -20,7 +20,12 @@ CREATE TABLE Istituto (
                           nome varchar(20) NOT NULL,
                           provincia char(2) NOT NULL,
                           collabora boolean NOT NULL
-                              DEFAULT FALSE
+                              DEFAULT FALSE,
+                          CHECK(provincia IN ('AG', 'AL', 'AN', 'AO', 'AR', 'AP', 'AT', 'AV', 'BA', 'BT', 'BL', 'BN', 'BG', 'BI', 'BO', 'BZ', 'BS', 'BR', 'CA', 
+                          'CL', 'CB', 'CI', 'CE', 'CT', 'CZ', 'CH', 'CH', 'CO', 'CS', 'CR', 'KR', 'CN', 'EN', 'FM', 'FE', 'FI', 'FG', 'FC', 'FR', 'GE', 'GO', 'GR', 
+                          'IM', 'IS', 'SP', 'AQ', 'LT', 'LE', 'LC', 'LI', 'LO', 'LU', 'MC', 'MN', 'MS', 'MT', 'ME', 'MI', 'MO', 'MB', 'NA', 'NO', 'NU', 'OT', 'OR', 
+                          'PD', 'PA', 'PR', 'PV', 'PG', 'PU', 'PE', 'PC', 'PI', 'PT', 'PN', 'PZ', 'PO', 'RG', 'RA', 'RC', 'RE', 'RI', 'RN', 'RM', 'RO', 'SA', 'VS', 
+                          'SS', 'SV', 'SI', 'SR', 'SO', 'TA', 'TE', 'TR', 'TO', 'OG', 'TP', 'TN', 'TV', 'TS', 'UD', 'VA', 'VE', 'VB', 'VC', 'VR', 'VV', 'VI', 'VT')) 
 );
 
 CREATE TABLE ReferenteIstituto(
